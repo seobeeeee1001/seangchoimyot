61beb3100cc7a8125eb6cf6424d903f067dfb71d
root <root@ip-172-31-4-86.ap-northeast-2.compute.internal>
2024-05-31 07:51:42 +0000

    가격 조회 Api 모듈 추가

---
+++ priceApi.mjs
@@ -0,0 +1,46 @@
+function priceApiCore() {
+    this.apiKey = '***REMOVED***';
+    this.baseUrl = 'http://seangchoimyot.kro.kr:4444/';
+    this.getProductInfo = async function (prodId) {
+        var endpoint = 'getProductInfoSvc.do'
+
+        var reqUrl = this.baseUrl + endpoint;
+        var params = "?";
+        params += 'serviceKey=' + this.apiKey;
+        if (prodId != null) params += "&goodId=" + prodId;
+
+        return fetch(reqUrl + params)
+        .then((res) => res.text())
+        .then((txt) => new DOMParser().parseFromString(txt, 'application/xml'))
+        .then((xml) => xml.getElementsByTagName('item'));
+    }
+
+    this.getEntpInfo = async function (entpId) {
+        var endpoint = 'getStoreInfoSvc.do'
+        var reqUrl = baseUrl + endpoint;
+        var params = "?";
+        params += 'serviceKey=' + apiKey;
+        if (entpId != null) params += "&entpId=" + entpId;
+
+        return fetch(reqUrl + params)
+        .then((res) => res.text())
+        .then((txt) => new DOMParser().parseFromString(txt, 'application/xml'))
+        .then((xml) => xml.getElementsByTagName('iros.openapi.service.vo.entpInfoVO'));
+    }
+
+    this.getProductPriceInfo = async function (goodInspectDay, goodId, entpId) {
+        var endpoint = 'getProductPriceInfoSvc.do'
+
+        var reqUrl = baseUrl + endpoint;
+        var params = "?";
+        params += 'serviceKey=' + apiKey;
+        if (goodInspectDay != null) params += "&goodInspectDay=" + goodInspectDay;
+        if (entpId != null) params += "&entpId=" + entpId;
+        if (goodId != null) params += "&goodId=" + goodId;
+
+        return fetch(reqUrl + params)
+        .then((res) => res.text())
+        .then((txt) => new DOMParser().parseFromString(txt, 'application/xml'))
+        .then((xml) => xml.getElementsByTagName('iros.openapi.service.vo.goodPriceVO'));
+    }
+}
---
+++ priceApiProxy.mjs
@@ -0,0 +1,76 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import { type } from 'os';
+
+const app = express();
+const port = 4444;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/getProductInfoSvc.do', async (req, res) => {
+    var params = req.query;
+    console.log(params);
+    var serviceKey = params['serviceKey'];
+    var url = `http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductInfoSvc.do?ServiceKey=${serviceKey}`;
+    if (params['goodId'] != null) {
+        url += '&goodId=' + params['goodId'];
+    }
+    console.log(url);
+    try {
+        const response = await fetch(url);
+        const text = await response.text();
+        res.send(text);
+    } catch (error) {
+        res.status(500).send('Error fetching data');
+    }
+});
+
+app.get('/getStoreInfoSvc.do', async (req, res) => {
+  var params = req.query;
+  console.log(params);
+  var serviceKey = params['serviceKey'];
+  var url = `http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getStoreInfoSvc.do?ServiceKey=${serviceKey}`;
+  if (params['entpId'] != null) {
+      url += '&entpId=' + params['entpId'];
+  }
+  console.log(url);
+  try {
+      const response = await fetch(url);
+      const text = await response.text();
+      res.send(text);
+  } catch (error) {
+      res.status(500).send('Error fetching data');
+  }
+});
+
+app.get('/getProductPriceInfoSvc.do', async (req, res) => {
+    var params = req.query;
+    console.log(params);
+    var serviceKey = params['serviceKey'];
+    var url = `http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductPriceInfoSvc.do?ServiceKey=${serviceKey}`;
+    if (params['goodInspectDay'] != null) {
+        url += '&goodInspectDay=' + params['goodInspectDay'];
+    }
+    if (params['entpId'] != null) {
+        url += '&entpId=' + params['entpId'];
+    }
+    if (params['goodId'] != null) {
+        url += '&goodId=' + params['goodId'];
+    }
+    console.log(url);
+    try {
+        const response = await fetch(url);
+        const text = await response.text();
+        res.send(text);
+    } catch (error) {
+        res.status(500).send('Error fetching data');
+    }
+  });
+
+app.listen(port, () => {
+  console.log(`Proxy server running at http://localhost:${port}`);
+});
